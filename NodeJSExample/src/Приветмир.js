387c1c903defd421e0fb6ee13b556b032bfc0031
usman <muhammad usman>
2024-07-06 10:25:46 +0500

    first commit

---
+++ NodeJSExample/src/Приветмир.js
@@ -0,0 +1,7 @@
+// NodeJSExample/Приветмир.js
+function hello () {
+first_name = "Israel"
+console.log("Hello " + first_name)
+return ("Hello " + first_name)
+};
+module.exports = hello;
---
+++ NodeJSExample/tests/helloworld.test.js
@@ -0,0 +1,8 @@
+// Unit test
+const hello = require("../src/Приветмир");
+test("Returns Hello Israel", () => {
+    expect(hello()).toBe("Hello Israel");
+});
+
+
+// ask Bro Jensen about adding to team repository 
